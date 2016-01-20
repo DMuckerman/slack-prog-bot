3d6317409498f50156c21b3508594a25364a6082
Daniel Muckerman <danielmuckerman@me.com>
2016-01-19 19:30:46 -0500

    Made Ruby interpretation work
    :

--- Gemfile
+++ Gemfile
@@ -3,11 +3,8 @@ source 'http://rubygems.org'
 ruby '2.3.0'
 
 gem 'slack-ruby-bot', '~> 0.6.0'
-gem 'wolfram'
 gem 'puma'
 gem 'sinatra'
-gem 'mechanize'
-gem 'nokogiri'
 
 group :development, :test do
   gem 'rake'
--- slack-prog/commands/ruby.rb
+++ slack-prog/commands/ruby.rb
@@ -4,9 +4,32 @@ module SlackMathbot
     class Ruby < SlackRubyBot::Commands::Base
       match /\!ruby(?<code>.*)$/ do |client, _data, _match|
 
-        puts _match[:code]
+        # Strip code out of triple backtick block
+        command = _data.text.split("```").drop(1)[0]
+        # Combine all lines of code into a single string
+        #command.gsub! "\n", ";" # May be unnessecary
 
+        # Debug print code to be run
+        puts command
+
+        #command.gsub! "puts", ""
+        #command.gsub! "print", ""
+
+        # If someone (Ryan decides to be an asshat)
+        if (command.include? "while true") || (command.include? "while 1")
+          result = "Fuck off, Ryan!"
+        else # Otherwise, eval the cod
+          result = eval(command).to_s.chomp.strip
+          result = eval("begin $stdout = StringIO.new; #{command}; $stdout.string;
+ensure $stdout = STDOUT end")
+        end
+
+        # Debug print the result
+        puts result
+
+        client.say(text: "```" + result + "```", channel: _data.channel)
+        
       end
     end
   end
- end
+end
